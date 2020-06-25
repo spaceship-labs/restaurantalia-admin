3bf068e61d8631010aeb1481005a5a8ba5058f5b
erick <erick@spaceshiplabs.com>
2020-06-25 18:41:49 -0500

    remove static components from allcomponents

--- src/components/allcomponents.js
+++ src/components/allcomponents.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import HeadComponent from './head';
-import TableComponent from './table';
 import MenusContainer from '../containers/menus';
 
 const AllComponentsList = () => (
   <>
-    <HeadComponent
-      title="Home container"
-      description="Lorem ipsum dolor sit amet"
-    />
-    <TableComponent title="algo" />
     <MenusContainer />
   </>
 );
