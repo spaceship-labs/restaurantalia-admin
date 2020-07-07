d3fc913f1b7d2d1e5add489716056ca2010333e6
erick <erick@spaceshiplabs.com>
2020-07-06 21:33:24 -0500

    login image

--- src/containers/login/index.styled.js
+++ src/containers/login/index.styled.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 import { Grid } from '@material-ui/core';
+import loginBg from '../../theme/images/login.jpg';
 
 export const GridContainer = styled(Grid)`
   height: 100vh;
 `;
 
 export const GridImage = styled(Grid)`
-  background-image: url(https://source.unsplash.com/random);
+  /* background-image: url(https://source.unsplash.com/random); */
+  background-image: url(${loginBg});
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
