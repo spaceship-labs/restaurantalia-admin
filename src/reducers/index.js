74b421b19c00425e10aedd12aeb25efb19212449
Pablo Melendez <pablofer.melendez@gmail.com>
2020-07-15 16:56:30 -0500

    update api files and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import auth from './auth';
 import menus from './restaurants';
 import categories from './categories';
 import dishes from './dishes';
+import menu from './menu';
 
 const reducer = combineReducers({
   auth,
   menus,
+  menu,
   categories,
   dishes,
 });
